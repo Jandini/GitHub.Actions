1d4fbbf9c58ff85c86e5df2937384aead39f3707
Matt Janda <matt.janda.kingston@gmail.com>
2021-05-31 07:42:14 +0100

    feat: Add sample source code

---
+++ src/GitHub.Actions/Enumeration.Template.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Janda.Base
+{
+    public abstract class Enumeration<T> : IComparable<T>, IEquatable<T> where T : Enumeration<T>
+    {
+        public int Value { get; private set; }
+        public string Name { get; private set; }
+        public override string ToString() => Name;
+
+        protected Enumeration(int value, string name)
+        {
+            Value = value;
+            Name = name;
+        }
+
+        public bool Equals(T other)
+        {
+            return other.Value.Equals(Value);
+        }
+
+        public int CompareTo(T other)
+        {
+            return Value.CompareTo(other.Value);
+        }
+    }
+}
---
+++ src/GitHub.Actions/Enumeration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Janda.Base
+{
+    public abstract class Enumeration : IComparable
+    {
+        public int Value { get; private set; }
+        public string Name { get; private set; }
+        public override string ToString() => Name;
+
+        protected Enumeration(int value, string name)
+        {
+            Value = value;
+            Name = name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Enumeration otherValue))
+                return false;
+
+            var typeMatches = GetType().Equals(obj.GetType());
+            var valueMatches = Value.Equals(otherValue.Value);
+
+            return typeMatches && valueMatches;
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        public int CompareTo(object other)
+        {
+            return Value.CompareTo(((Enumeration)other).Value);
+        }
+    }
+}
